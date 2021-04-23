8693fc1469e4fe9d4b989b345cf566dd2609733f
Aarón Contreras <aaron@redradix.com>
2021-04-23 16:06:29 +0200

    Add tests for unique name condition when creating users

--- users-service/test.js
+++ users-service/test.js
@@ -28,12 +28,31 @@ describe('Dummy test', () => {
         password: 'mypassword'
       }
       const res = await request(app).post('/users').send(payload)
+
       const { status, text } = res
       const body = JSON.parse(text)
 
       expect(status).toEqual(200)
       expect(body).toHaveProperty('message', 'user created' )
       expect(body).toHaveProperty('id')
+    })
+
+    it('Cannot create users with the same username', async () => {
+      const payload = {
+        username: 'Aaron',
+        email: 'aaron@redradix.com',
+        password: 'mypassword'
+      }
+      await request(app).post('/users').send(payload)
+      const res = await request(app).post('/users').send(payload)
+
+      const { status, text } = res
+      const body = JSON.parse(text)
+
+      expect(status).toEqual(400)
+      expect(body).toHaveProperty('errors')
+      expect(body.errors).toHaveLength(1)
+      expect(body.errors[0]).toBe('username already in use')
     })
   })
 
