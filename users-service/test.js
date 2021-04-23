04e62a3024f09586318ed046ac15075df395c3f1
Aarón Contreras <aaron@redradix.com>
2021-04-23 15:52:16 +0200

    Add test for POST /users happy path

--- users-service/test.js
+++ users-service/test.js
@@ -13,4 +13,21 @@ describe('Dummy test', () => {
     expect(JSON.parse(text)).toEqual({ message: 'aupa ahi' })
   })
 
+  describe('POST /users', () => {
+    it('Returns 200 if everything goes ok', async () => {
+      const payload = {
+        username: 'Aaron',
+        email: 'aaron@redradix.com',
+        password: 'mypassword'
+      }
+      const res = await request(app).post('/users').send(payload)
+      const { status, text } = res
+      const body = JSON.parse(text)
+
+      expect(status).toEqual(200)
+      expect(body).toHaveProperty('message', 'user created' )
+      expect(body).toHaveProperty('id')
+    })
+  })
+
 })
